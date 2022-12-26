21e7b861f54c8b6321e5f2e1bcfae35430ef1650
SJY <17839167062@163.com>
2022-12-26 09:20:03 +0800

    B

--- Base/src/main/java/com/inusun/git_test/base/Test.java
+++ Base/src/main/java/com/inusun/git_test/base/Test.java
@@ -2,7 +2,7 @@ package com.inusun.git_test.base;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("A");
+        System.out.println("C");
         System.out.println("B");
     }
 }
